890e685f3ec569dcffb9e59711a996bd6aeea83e
Minatrix <tonorio@yandex.ru>
2015-04-09 20:49:17 +0500

    Созданы папки models, views, controllers. Изменен database.php

---
+++ controllers/index.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '../views/news.php';
+require __DIR__ . '../models/news_form.php';
+require_once __DIR__ . '../models/database.php';
+?>
---
+++ views/news.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Новости о самом главном</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1><a href="/news-one.html" name="news-one">Петербуржец переклеивал штрих-коды в магазине, чтобы сэкономить
+        на продуктах</a></h1>
+<h1><a href="/news-two.html" name="news-two">Рубль стал лучшей валютой в мире</a></h1>
